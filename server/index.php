4168b1901da0bc6af78851b46cf8ff1624f8af89
Guillaume RYCKELYNCK <guillaume.ryckelynck@region-alsace.eu>
2016-03-16 09:15:13 +0100

    Update index.php

--- server/index.php
+++ server/index.php
@@ -7,7 +7,6 @@ $url = '';
 if (isset($_GET['url'])) {
     $url = $_GET['url'];
 }
-/*
 // Create a stream
 $opts = array(
     'http'=>array(
@@ -22,10 +21,9 @@ $opts = array(
 );
 // Create context options
 $context = stream_context_create($opts);
-*/
 if ($url) {
-    echo file_get_contents($url);
-    // echo file_get_contents($url, false, $context);
+    //echo file_get_contents($url);
+    echo file_get_contents($url, false, $context);
 } else {
     echo false;
 }
